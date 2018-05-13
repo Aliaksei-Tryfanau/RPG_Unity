9eea4bf5f5b48c2ffc8b31f62045989ce2eda8dc
Aleksey Trifonov <altrishka@gmail.com>
2018-05-13 09:52:45 +0300

    Initial commit

---
+++ Assets/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace nani{
+    public class Utility : MonoBehaviour
+    {
+
+    }
+    public enum Layer
+    {
+        Walkable = 8, Enemy = 9, RaycastEndStop = -1
+    }
+}
