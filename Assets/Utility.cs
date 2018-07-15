922775833890d31e54f89bdc7578ca7bae9652a6
Aleksey Trifonov <altrishka@gmail.com>
2018-07-15 22:58:55 +0300

    Player movement and cursor done

--- Assets/Utility.cs
+++ Assets/Utility.cs
@@ -1,15 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-namespace nani{
-    public class Utility : MonoBehaviour
-    {
-
-    }
-    public enum Layer
-    {
-        Walkable = 8, Enemy = 9, RaycastEndStop = -1
-    }
+﻿public enum Layer {
+    Walkable = 8,
+    Enemy = 9,
+    RaycastEndStop = -1
 }
